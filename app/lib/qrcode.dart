014f6d61570a0435eaf0b7f49b843c5af1d6b03f
Ilya Siganov <ilya.blan4@gmail.com>
2023-11-06 12:34:28 +0600

    feat: add qr code scanner

---
+++ app/lib/qrcode.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
+
+class QRCodePage extends StatelessWidget {
+  const QRCodePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Mobile Scanner')),
+      body: MobileScanner(
+        // fit: BoxFit.contain,
+        onDetect: (capture) {
+          final List<Barcode> barcodes = capture.barcodes;
+          for (final barcode in barcodes) {
+            debugPrint('Barcode found! ${barcode.rawValue}');
+          }
+        },
+      ),
+    );
+  }
+}
