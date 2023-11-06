2e8638d22ff1abb8dcd6fa0944a1f5d92e974fee
Ilya Siganov <ilya.blan4@gmail.com>
2023-11-06 15:21:08 +0600

    feat: add chi router

--- server/go.mod
+++ server/go.mod
@@ -2,6 +2,9 @@ module server
 
 go 1.21.2
 
-require github.com/gorilla/websocket v1.5.1
+require (
+	github.com/go-chi/chi/v5 v5.0.10
+	github.com/gorilla/websocket v1.5.1
+)
 
 require golang.org/x/net v0.17.0 // indirect
--- server/main.go
+++ server/main.go
@@ -7,7 +7,10 @@ import (
 	"log"
 	"net/http"
 	"server/randomness"
+	"strings"
 
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/gorilla/websocket"
 )
 
@@ -21,60 +24,89 @@ type SessionCodeMessage struct {
 }
 
 var upgrader = websocket.Upgrader{}
+var connections map[string]*websocket.Conn = make(map[string]*websocket.Conn)
 
 func generateSessionCode() (string, error) {
 	token, err := randomness.GenerateRandomString(6)
 	return token, err
 }
 
-func sendSessionCode() {
+func wsHandler(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer conn.Close()
+	log.Println("Connection!")
 
+	code, err := generateSessionCode()
+	if err != nil {
+		log.Printf("[ERROR] failed to generate code: %v", err)
+		return
+	}
+	log.Println(code)
+	connections[code] = conn
+	defer func() {
+		delete(connections, code)
+	}()
+
+	err = conn.WriteJSON(Message{Type: "session_code", Data: SessionCodeMessage{Code: code}})
+	if err != nil {
+		log.Printf("[ERROR] failed to send message to client")
+	}
+
+	for {
+		// Read message from browser
+		msgType, msg, err := conn.ReadMessage()
+		if err != nil {
+			log.Println(err)
+			break
+		}
+
+		// Print the message to the console
+		log.Printf("%s sent(%d): %s\n", conn.RemoteAddr(), msgType, string(msg))
+
+		// Write message back to browser
+		// if err = conn.WriteMessage(msgType, msg); err != nil {
+		// 	log.Println(err)
+		// 	break
+		// }
+	}
+}
+
+// FileServer conveniently sets up a http.FileServer handler to serve
+// static files from a http.FileSystem.
+func FileServer(r chi.Router, path string, root http.FileSystem) {
+	if strings.ContainsAny(path, "{}*") {
+		panic("FileServer does not permit any URL parameters.")
+	}
+
+	if path != "/" && path[len(path)-1] != '/' {
+		r.Get(path, http.RedirectHandler(path+"/", 301).ServeHTTP)
+		path += "/"
+	}
+	path += "*"
+
+	r.Get(path, func(w http.ResponseWriter, r *http.Request) {
+		rctx := chi.RouteContext(r.Context())
+		pathPrefix := strings.TrimSuffix(rctx.RoutePattern(), "/*")
+		fs := http.StripPrefix(pathPrefix, http.FileServer(root))
+		fs.ServeHTTP(w, r)
+	})
 }
 
 func main() {
-	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
-		conn, err := upgrader.Upgrade(w, r, nil)
-		if err != nil {
-			log.Println(err)
-			return
-		}
-		defer conn.Close()
-		log.Println("Connection!")
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.HandleFunc("/ws", wsHandler)
 
-		code, err := generateSessionCode()
-		if err != nil {
-			log.Printf("[ERROR] failed to generate code: %v", err)
-			return
-		}
-		log.Println(code)
+	FileServer(r, "/media", http.Dir("./media"))
 
-		err = conn.WriteJSON(Message{Type: "session_code", Data: SessionCodeMessage{Code: code}})
-		if err != nil {
-			log.Printf("[ERROR] failed to send message to client")
-		}
-
-		for {
-			// Read message from browser
-			msgType, msg, err := conn.ReadMessage()
-			if err != nil {
-				log.Println(err)
-				break
-			}
-
-			// Print the message to the console
-			log.Printf("%s sent(%d): %s\n", conn.RemoteAddr(), msgType, string(msg))
-
-			// Write message back to browser
-			// if err = conn.WriteMessage(msgType, msg); err != nil {
-			// 	log.Println(err)
-			// 	break
-			// }
-		}
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello!"))
 	})
 
-	fs := http.FileServer(http.Dir("./media"))
-	http.Handle("/media/", http.StripPrefix("/media/", fs))
-
 	log.Println("Server is running on http://localhost:8000")
-	log.Fatal(http.ListenAndServe(":8000", nil))
+	log.Fatal(http.ListenAndServe(":8000", r))
 }
